9949ff9fce6fa67dba0b66a05049e5400fab29f5
srp <sharathreddyp@gmail.com>
2016-06-30 12:24:11 -0500

    updated

--- array/ZigZagArrangemnt.cs
+++ array/ZigZagArrangemnt.cs
@@ -1,53 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LINQPad;
-namespace ADS.array
+﻿using LINQPad;
+// Given an array of unique elements rearrange the array to be a < b > c < d > e form
+public class ZigZagArrangemnt
 {
-    // Given an array of unique elements rearrange the array to be a < b > c < d > e form
-    public class ZigZagArrangemnt
+    private void ArrangeZigZag(int[] array)
     {
-        private void ArrangeZigZag(int[] array)
+        //12345 ? duplicates-no? negative numbers?
+
+        bool isLess = true;
+
+        for (int i = 0; i < array.Length - 1; i++)
         {
-            //12345 ? duplicates-no? negative numbers?
-
-            bool isLess = true;
-
-            for (int i = 0; i < array.Length - 1; i++)
+            if (isLess)
             {
-                if (isLess)
+                if (array[i] > array[i + 1])
                 {
-                    if (array[i] > array[i + 1])
-                    {
-                        swap(array, i, i + 1);
-                    }
+                    swap(array, i, i + 1);
                 }
-                else
-                {
-                    if (array[i] < array[i + 1])
-                    {
-                        swap(array, i, i + 1);
-                    }
-                }
-                isLess = !isLess;
             }
+            else
+            {
+                if (array[i] < array[i + 1])
+                {
+                    swap(array, i, i + 1);
+                }
+            }
+            isLess = !isLess;
+        }
 
-        }
-        private void swap(int[] array, int firstIndex, int secondIndex)
-        {
-            int temp = array[firstIndex];
-            array[firstIndex] = array[secondIndex];
-            array[secondIndex] = temp;
-        }
-
-        public void Test()
-        {
-            int[] array = new int[] { 4, 5, 3, 6, 7, 9 };
-            ArrangeZigZag(array);
-            array.Dump();
-        }
+    }
+    private void swap(int[] array, int firstIndex, int secondIndex)
+    {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
     }
 
+    public void Test()
+    {
+        int[] array = new int[] { 4, 5, 3, 6, 7, 9 };
+        ArrangeZigZag(array);
+        array.Dump();
+    }
 }
